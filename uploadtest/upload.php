1f3634ac60dc40b82beba7f81e002b6cce7995c5
scotusrbx <30281108+scotusrbx@users.noreply.github.com>
2018-02-13 15:50:00 -0800

    Add files via upload

---
+++ uploadtest/upload.php
@@ -0,0 +1,17 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$docFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if(isset($_POST["submit"])) {
+    $check = ($docFileType == "pdf"));
+    if($check) {
+        echo "File is a PDF - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not a PDF.";
+        $uploadOk = 0;
+    }
+}
+?>
